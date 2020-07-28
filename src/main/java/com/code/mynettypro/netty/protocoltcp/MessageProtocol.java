ae636b1d88c4c70da29127874dbbf2f449def740
lisensen <641307158@qq.com>
2020-07-28 21:09:00 +0800

    add all

---
+++ src/main/java/com/code/mynettypro/netty/protocoltcp/MessageProtocol.java
@@ -0,0 +1,26 @@
+package com.code.mynettypro.netty.protocoltcp;
+
+/**
+ * TCP粘包和拆包
+ */
+//协议包
+public class MessageProtocol {
+    private int len; //关键
+    private byte[] content;
+
+    public int getLen() {
+        return len;
+    }
+
+    public void setLen(int len) {
+        this.len = len;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+}
